1970c80d29094438f2ee77f7a24b2a06dc067cb5
shenxf1986 <185693240@qq.com>
2019-08-16 13:05:55 +0800

    update

--- ts-axios/examples/base/app.ts
+++ ts-axios/examples/base/app.ts
@@ -1,66 +1,66 @@
 import axios from '../../src/index'
 
-// axios({
-//   method: 'get',
-//   url: '/base/get',
-//   params: {
-//     foo: ['bar', 'baz']
-//   }
-// })
-//
-// axios({
-//   method: 'get',
-//   url: '/base/get',
-//   params: {
-//     foo: {
-//       bar: 'baz'
-//     }
-//   }
-// })
-//
-// const date = new Date()
-//
-// axios({
-//   method: 'get',
-//   url: '/base/get',
-//   params: {
-//     date
-//   }
-// })
-//
-// axios({
-//   method: 'get',
-//   url: '/base/get',
-//   params: {
-//     foo: '@:$, '
-//   }
-// })
-//
-// axios({
-//   method: 'get',
-//   url: '/base/get',
-//   params: {
-//     foo: 'bar',
-//     baz: null
-//   }
-// })
-//
-// axios({
-//   method: 'get',
-//   url: '/base/get#hash',
-//   params: {
-//     foo: 'bar'
-//   }
-// })
-//
-// axios({
-//   method: 'get',
-//   url: '/base/get?foo=bar',
-//   params: {
-//     bar: 'baz'
-//   }
-// })
-//
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: ['bar', 'baz']
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: {
+      bar: 'baz'
+    }
+  }
+})
+
+const date = new Date()
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    date
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: '@:$, '
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: 'bar',
+    baz: null
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get#hash',
+  params: {
+    foo: 'bar'
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get?foo=bar',
+  params: {
+    bar: 'baz'
+  }
+})
+
 // axios({
 //   method: 'post',
 //   url: '/base/post',
@@ -69,7 +69,7 @@ import axios from '../../src/index'
 //     b: 2
 //   }
 // })
-//
+
 // axios({
 //   method: 'post',
 //   url: '/base/post',
@@ -81,44 +81,44 @@ import axios from '../../src/index'
 //     b: 2
 //   }
 // })
-//
+
 // const arr = new Int32Array([21, 31])
-//
+
 // axios({
 //   method: 'post',
 //   url: '/base/buffer',
 //   data: arr
 // })
-//
-//
+
+
 // const paramsString = 'q=URLUtils.searchParams&topic=api'
 // const searchParams = new URLSearchParams(paramsString)
-//
+
 // axios({
 //   method: 'post',
 //   url: '/base/post',
 //   data: searchParams
 // })
 
-axios({
-  method: 'post',
-  url: '/base/post',
-  data: {
-    a: 1,
-    b: 2
-  }
-}).then((res) => {
-  console.log(res)
-})
+// axios({
+//   method: 'post',
+//   url: '/base/post',
+//   data: {
+//     a: 1,
+//     b: 2
+//   }
+// }).then((res) => {
+//   console.log(res)
+// })
 
-axios({
-  method: 'post',
-  url: '/base/post',
-  responseType: 'json',
-  data: {
-    a: 3,
-    b: 4
-  }
-}).then((res) => {
-  console.log(res)
-})
+// axios({
+//   method: 'post',
+//   url: '/base/post',
+//   responseType: 'json',
+//   data: {
+//     a: 3,
+//     b: 4
+//   }
+// }).then((res) => {
+//   console.log(res)
+// })
--- ts-axios/src/index.ts
+++ ts-axios/src/index.ts
@@ -1,8 +1,19 @@
 import { AxiosRequestConfig } from './types';
+import { buildURL } from './helpers/url';
 import xhr from './xhr';
 
-function axios(config: AxiosRequestConfig) {
+function axios(config: AxiosRequestConfig): void {
+  processConfig(config);
   xhr(config);
+}
+
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformUrl(config);
+}
+
+function transformUrl(config: AxiosRequestConfig): string {
+  const { url, params } = config;
+  return buildURL(url, params);
 }
 
 export default axios;
