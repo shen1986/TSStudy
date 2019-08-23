c64a2645369e17baf414481ba31226bfcd679b44
shenxf1986 <185693240@qq.com>
2019-08-23 09:04:31 +0800

    update

--- ts-axios/src/index.ts
+++ ts-axios/src/index.ts
@@ -1,12 +1,14 @@
-import { AxiosRequestConfig, AxiosPromise } from './types';
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types';
 import { buildURL } from './helpers/url';
 import xhr from './xhr';
-import { transformRequest } from './helpers/data';
+import { transformRequest, transformResponse } from './helpers/data';
 import { processHeaders } from './helpers/header';
 
 function axios(config: AxiosRequestConfig): AxiosPromise {
   processConfig(config);
-  return xhr(config);
+  return xhr(config).then((res) => {
+    return transformResponseData(res);
+  })
 }
 
 function processConfig(config: AxiosRequestConfig): void {
@@ -27,6 +29,11 @@ function transformRequestData(config: AxiosRequestConfig): any {
 function transformHeaders(config: AxiosRequestConfig) {
   const {headers = {}, data} = config;
   return processHeaders(headers, data);
+}
+
+function transformResponseData(res: AxiosResponse): AxiosResponse {
+  res.data = transformResponse(res.data)
+  return res;
 }
 
 export default axios;
