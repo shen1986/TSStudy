09bd5ab181c32be96a740ce4343437e4126d8ed1
shenxf1986 <185693240@qq.com>
2019-09-20 09:55:53 +0800

    update

--- ts-axios/src/types/index.ts
+++ ts-axios/src/types/index.ts
@@ -90,6 +90,7 @@ export interface AxiosRequestConfig {
   xsrfCookieName?: string
   xsrfHeaderName?: string
   auth?: AxiosBasicCredentials
+  paramsSerializer?: (params: any) => string
   validateStatus?: (status: number) => boolean
   onDownloadProgress?: (e: ProgressEvent) => void
   onUploadProgress?: (e: ProgressEvent) => void
