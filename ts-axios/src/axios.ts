31d598be6869eb612e9a5e80f44389ae7ab9a31a
shenxf1986 <185693240@qq.com>
2019-09-23 09:40:34 +0800

    update

--- ts-axios/src/axios.ts
+++ ts-axios/src/axios.ts
@@ -5,6 +5,7 @@ import defaults from './default';
 import mergeConfig from './core/mergeConfig';
 import CancelToken from './cancel/CancelToken';
 import Cancel, { isCancel } from './cancel/Cancel';
+import { AxiosError } from './helpers/error';
 
 function createInstance(config: AxiosRequestConfig): AxiosStatic {
   const context = new Axios(config);
@@ -24,5 +25,17 @@ axios.create = function create(config) {
 axios.CancelToken = CancelToken
 axios.Cancel = Cancel
 axios.isCancel = isCancel
+
+axios.all = function all(promises) {
+  return Promise.all(promises)
+}
+
+axios.spread = function spread(callback) {
+  return function wrap(arr) {
+    return callback.apply(null, arr);
+  }
+}
+
+axios.Axios = Axios
 
 export default axios;
